52d06fecc55dc90383aaf12f44e1fd174724aa2b
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2020-09-08 21:56:16 -0300

    working

--- src/main/kotlin/com/alexandreesl/Application.kt
+++ src/main/kotlin/com/alexandreesl/Application.kt
@@ -1,2 +1,38 @@
 package com.alexandreesl
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+
+object Application {
+
+    private val logger = KotlinLogging.logger {}
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+
+        val deferred = CoroutineScope(IO).async {
+            logger.info { "running on ${Thread.currentThread().name}" }
+            delay(2000)
+            123
+        }
+
+        runBlocking {
+
+            logger.info { "running on ${Thread.currentThread().name}" }
+
+            val resp = deferred.await()
+
+            logger.info { "the result is $resp" }
+
+        }
+
+
+
+    }
+
+}
